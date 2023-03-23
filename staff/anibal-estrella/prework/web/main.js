b007da0fb2b2ad18c53e9b22ecdedd20f9e88c8e
anibal estrella <nbl@NBL.local>
2023-03-23 19:35:12 +0100

    Logic nd L&F

--- staff/anibal-estrella/prework/web/main.js
+++ staff/anibal-estrella/prework/web/main.js
@@ -1,16 +1,60 @@
-document.querySelector(".register").querySelector("form").addEventlistener("submit", function (event) {
-    event.preventsDefault();
+//DATA
+var users=[]
+
+users.push({
+  name:'Lars Ulrich',
+  email:'lars@gmail.com',
+  password:'asdfasdfewr',
+});
+
+users.push({
+  name:'Eddie Veder',
+  email:'veder@gmail.com',
+  password:'asdfasdfewr',
+});
+
+users.push({
+  name:'Bruce Dickinson',
+  email:'iron@gmail.com',
+  password:'asdfasdfewr',
+});
+// LOGIC
+// PRESENTATION 
+
+var foundUser;
+
+document.querySelector('.login form').addEventListener('submit', function (event) {
+  event.preventDefault()
+    event.preventDefault();
     document.querySelector(".register").classList.add("off");
     document.querySelector(".login").classList.remove("off");
+console.log('FUCK YOU!!')
+for (let i = 0; i < users.length; i++) {
+  var user = users[i];
+  if (user.email === email) {
+    foundUser =user;
+    break;
+  };
 
-    console.log("Register!");
-    // TODO how to get
-  });
-
-document.querySelector(".login").addEventlistener("submit", function (event) {
-  event.preventsDefault();
-  document.querySelector(".").classList.add("off");
-  document.querySelector(".").classList.remove("on");
-  console.log("Login!");
-  // TODO how to get
+  
+}
 });
+
+// document.querySelector(".login form").addEventlistener("submit", function (event) {
+//   event.preventsDefault();
+//   document.querySelector(".").classList.add("off");
+//   document.querySelector(".").classList.remove("off");
+
+//   var email = document.querySelector('input[name]').value
+//   var password = document.querySelector('input[password]')
+//   console.log(email, name)
+// });
+
+// document.querySelector(".register a").addEventlistener("click", function (event) {
+//   event.preventsDefault();
+//   document.querySelector(".register").classList.add("off");
+//   document.querySelector(".login").classList.remove("off");
+//   console.log("Login!");
+//   // TODO how to get
+// });
+
